9ba3736e314fbd9fa6e20935ea0824521aa42cba
Dora Xiao <dlxiao@LAPTOP-DUF7JNPL.localdomain>
2022-10-05 12:07:02 -0400

    add default values to database columns

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,3 +1,3 @@
-alter table T_DOCUMENT add column scoresum int;
-alter table T_DOCUMENT add column scorecount int;
+alter table T_DOCUMENT add column scoresum int default 0 not null;
+alter table T_DOCUMENT add column scorecount int default 0 not null;
 update T_CONFIG set CFG_VALUE_C = '28' where CFG_ID_C = 'DB_VERSION';
